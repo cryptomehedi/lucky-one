52602c79c96e652066132c804ed0d132cbd5d80b
cryptomehedi <classicalmehedi@gmail.com>
2022-03-26 01:38:20 +0600

    sec commit

--- src/Component/Shop/Shop.js
+++ src/Component/Shop/Shop.js
@@ -4,11 +4,28 @@ import Product from '../Product/Product';
 
 const Shop = () => {
     const [products, setProducts] = useState([])
+    const [cart , setCart] = useState([])
     useEffect( () => {
         fetch('data.json')
         .then(res => res.json())
         .then(data => setProducts(data))
     }, [])
+
+    const addToCart = (selectedProduct) => {
+        let newCart;
+        const existing = cart.find(product => product.id === selectedProduct.id);
+        if (!existing) {
+            // selectedProduct.quantity = 1
+            newCart = [...cart , selectedProduct]
+        }
+        else{
+            const rest = cart.filter(product => product.id !== selectedProduct.id);
+            // existing.quantity = existing.quantity + 1
+            newCart = [...rest , existing]
+        }
+        console.log(newCart);
+        setCart(newCart)
+    }
 
     return (
         <div className="grid grid-cols-shop_col w-auto">
@@ -17,6 +34,7 @@ const Shop = () => {
                     products.map(product => <Product 
                         product={product}
                         key = {product.id}
+                        addToCart = {addToCart}
                         ></Product>
                     )
                 }
